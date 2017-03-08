49743c3e65649133d05a42ca0f71b59f2b88dd42
Emad Saber <emad.saber@sewedy.com.eg>
2017-03-08 09:14:15 +0200

    Initial Commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace TicTacToe
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnl_0_0 = new System.Windows.Forms.Panel();
+            this.pnl_0_1 = new System.Windows.Forms.Panel();
+            this.pnl_0_2 = new System.Windows.Forms.Panel();
+            this.pnl_1_0 = new System.Windows.Forms.Panel();
+            this.pnl_1_1 = new System.Windows.Forms.Panel();
+            this.pnl_1_2 = new System.Windows.Forms.Panel();
+            this.pnl_2_0 = new System.Windows.Forms.Panel();
+            this.pnl_2_1 = new System.Windows.Forms.Panel();
+            this.pnl_2_2 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.pnl_0_0, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pnl_0_1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pnl_0_2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pnl_1_0, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pnl_1_1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pnl_1_2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pnl_2_0, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pnl_2_1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pnl_2_2, 2, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(429, 382);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pnl_0_0
+            // 
+            this.pnl_0_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_0_0.Location = new System.Drawing.Point(6, 6);
+            this.pnl_0_0.Name = "pnl_0_0";
+            this.pnl_0_0.Size = new System.Drawing.Size(132, 117);
+            this.pnl_0_0.TabIndex = 0;
+            // 
+            // pnl_0_1
+            // 
+            this.pnl_0_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_0_1.Location = new System.Drawing.Point(147, 6);
+            this.pnl_0_1.Name = "pnl_0_1";
+            this.pnl_0_1.Size = new System.Drawing.Size(132, 117);
+            this.pnl_0_1.TabIndex = 0;
+            // 
+            // pnl_0_2
+            // 
+            this.pnl_0_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_0_2.Location = new System.Drawing.Point(288, 6);
+            this.pnl_0_2.Name = "pnl_0_2";
+            this.pnl_0_2.Size = new System.Drawing.Size(135, 117);
+            this.pnl_0_2.TabIndex = 0;
+            // 
+            // pnl_1_0
+            // 
+            this.pnl_1_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_1_0.Location = new System.Drawing.Point(6, 132);
+            this.pnl_1_0.Name = "pnl_1_0";
+            this.pnl_1_0.Size = new System.Drawing.Size(132, 117);
+            this.pnl_1_0.TabIndex = 0;
+            // 
+            // pnl_1_1
+            // 
+            this.pnl_1_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_1_1.Location = new System.Drawing.Point(147, 132);
+            this.pnl_1_1.Name = "pnl_1_1";
+            this.pnl_1_1.Size = new System.Drawing.Size(132, 117);
+            this.pnl_1_1.TabIndex = 0;
+            // 
+            // pnl_1_2
+            // 
+            this.pnl_1_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_1_2.Location = new System.Drawing.Point(288, 132);
+            this.pnl_1_2.Name = "pnl_1_2";
+            this.pnl_1_2.Size = new System.Drawing.Size(135, 117);
+            this.pnl_1_2.TabIndex = 0;
+            // 
+            // pnl_2_0
+            // 
+            this.pnl_2_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_2_0.Location = new System.Drawing.Point(6, 258);
+            this.pnl_2_0.Name = "pnl_2_0";
+            this.pnl_2_0.Size = new System.Drawing.Size(132, 118);
+            this.pnl_2_0.TabIndex = 0;
+            // 
+            // pnl_2_1
+            // 
+            this.pnl_2_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_2_1.Location = new System.Drawing.Point(147, 258);
+            this.pnl_2_1.Name = "pnl_2_1";
+            this.pnl_2_1.Size = new System.Drawing.Size(132, 118);
+            this.pnl_2_1.TabIndex = 0;
+            // 
+            // pnl_2_2
+            // 
+            this.pnl_2_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_2_2.Location = new System.Drawing.Point(288, 258);
+            this.pnl_2_2.Name = "pnl_2_2";
+            this.pnl_2_2.Size = new System.Drawing.Size(135, 118);
+            this.pnl_2_2.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(429, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game...";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 406);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Tic-Tac-Toe";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel pnl_0_0;
+        private System.Windows.Forms.Panel pnl_0_1;
+        private System.Windows.Forms.Panel pnl_0_2;
+        private System.Windows.Forms.Panel pnl_1_0;
+        private System.Windows.Forms.Panel pnl_1_1;
+        private System.Windows.Forms.Panel pnl_1_2;
+        private System.Windows.Forms.Panel pnl_2_0;
+        private System.Windows.Forms.Panel pnl_2_1;
+        private System.Windows.Forms.Panel pnl_2_2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class frmMain : Form
+    {
+        #region Enums
+        enum Shape
+        {
+            X = 0,
+            O = 1,
+        }
+        #endregion
+
+        #region Properties
+        private Shape NextShape { set; get; }
+        private bool GameEnded { set; get; }
+        #endregion
+
+        #region Constructors
+        public frmMain()
+        {
+            InitializeComponent();
+            Init();
+        } 
+        #endregion
+
+        #region Methods
+        private bool Draw(Panel pnl)
+        {
+            if (this.NextShape == Shape.X)
+            {
+                if (!DrawShape(pnl, TicTacToe.Properties.Resources.X)) return false;
+                this.NextShape = Shape.O;
+            }
+            else
+            {
+                if (!DrawShape(pnl, TicTacToe.Properties.Resources.O)) return false;
+                this.NextShape = Shape.X;
+            }
+            return true;
+        }
+        private bool DrawShape(Panel pnl, Bitmap shape)
+        {
+            pnl.BackgroundImage = shape;
+            pnl.BackgroundImageLayout = ImageLayout.Stretch;
+            pnl.Tag = this.NextShape.ToString(); // because NextShape has not been changed yet, so the next shape is the current shape.
+            return true;
+        }
+        private void Init()
+        {
+            this.NextShape = Shape.X;
+            pnl_0_0.Click += new EventHandler(Panel_Click);
+            pnl_0_1.Click += new EventHandler(Panel_Click);
+            pnl_0_2.Click += new EventHandler(Panel_Click);
+            pnl_1_0.Click += new EventHandler(Panel_Click);
+            pnl_1_1.Click += new EventHandler(Panel_Click);
+            pnl_1_2.Click += new EventHandler(Panel_Click);
+            pnl_2_0.Click += new EventHandler(Panel_Click);
+            pnl_2_1.Click += new EventHandler(Panel_Click);
+            pnl_2_2.Click += new EventHandler(Panel_Click);
+        }
+        private bool CheckEndOfGame(string row, string column)
+        {
+            bool res = false;
+            int rowNum = Convert.ToInt32(row);
+            int colNum = Convert.ToInt32(column);
+
+            res = ValidateRow(rowNum);
+            res |= ValidateColumn(colNum);
+            if ((rowNum == 0 && colNum == 0) ||
+                (rowNum == 1 && colNum == 1) ||
+                (rowNum == 2 && colNum == 2))
+            {
+                res |= ValidateCells(pnl_0_0, pnl_1_1, pnl_2_2);
+            }
+            if ((rowNum == 0 && colNum == 2) ||
+                (rowNum == 1 && colNum == 1) ||
+                (rowNum == 2 && colNum == 0))
+            {
+                res |= ValidateCells(pnl_0_2, pnl_1_1, pnl_2_0);
+            }
+            return res;
+        }
+        private bool ValidateRow(int rowNumber)
+        {
+            return this.ValidateCells(
+                tableLayoutPanel1.Controls["pnl_" + rowNumber.ToString() + "_0"] as Panel,
+                tableLayoutPanel1.Controls["pnl_" + rowNumber.ToString() + "_1"] as Panel,
+                tableLayoutPanel1.Controls["pnl_" + rowNumber.ToString() + "_2"] as Panel);
+        }
+        private bool ValidateColumn(int columnNumber)
+        {
+            return this.ValidateCells(
+                tableLayoutPanel1.Controls["pnl_0_" + columnNumber.ToString()] as Panel,
+                tableLayoutPanel1.Controls["pnl_1_" + columnNumber.ToString()] as Panel,
+                tableLayoutPanel1.Controls["pnl_2_" + columnNumber.ToString()] as Panel);
+        }
+        private bool ValidateCells(Panel pnl0, Panel pnl1, Panel pnl2)
+        {
+            if (pnl0 == null || pnl0.BackgroundImage == null) return false;
+            if (pnl1 == null || pnl1.BackgroundImage == null) return false;
+            if (pnl2 == null || pnl2.BackgroundImage == null) return false;
+
+            if (pnl0.Tag == pnl1.Tag &&
+                pnl1.Tag == pnl2.Tag) return true;
+            return false;
+        }
+        private void ResetAll()
+        {
+            pnl_0_0.BackgroundImage = null;
+            pnl_0_1.BackgroundImage = null;
+            pnl_0_2.BackgroundImage = null;
+            pnl_1_0.BackgroundImage = null;
+            pnl_1_1.BackgroundImage = null;
+            pnl_1_2.BackgroundImage = null;
+            pnl_2_0.BackgroundImage = null;
+            pnl_2_1.BackgroundImage = null;
+            pnl_2_2.BackgroundImage = null;
+        }
+        #endregion
+
+        #region Events
+        private void Panel_Click(object sender, EventArgs e)
+        {
+            Panel pnl =  sender as Panel;
+            if (pnl.BackgroundImage != null) return;
+            if (GameEnded) return;
+            this.Draw(pnl);
+            if (CheckEndOfGame(pnl.Name.Split('_')[1], pnl.Name.Split('_')[2]))
+            {
+                this.GameEnded = true;
+                MessageBox.Show(string.Format("The {0} player wins the game", (this.NextShape == Shape.X) ? "O" : "X"));
+                ResetAll();
+                return;
+            }
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ResetAll();
+            this.GameEnded = false;
+        }
+        #endregion
+    }
+}
